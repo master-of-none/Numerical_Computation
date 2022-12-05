01f6f1fff433ed508468acf88a4af0b8a402dc00
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2022-12-05 10:47:59 -0800

    NewCommit

--- simpsons.c
+++ simpsons.c
@@ -2,7 +2,7 @@
 #include<math.h>
 
 // Define f(x). Hardcoding the value and can be changed as and when required
-#define f(x) (sin(3*x + 2) - 5*x*x)
+#define f(x) (x * cos(2*x))
 double simpsons(double,double,int);
 
 int main(){
@@ -12,25 +12,26 @@ int main(){
     int panels;
 
     // Entering the values for low,high and panels
-    printf("Enter lower value of the integral\n");
+    //printf("Enter lower value of the integral\n");
     //change value here
-    scanf("%lf",&low);
-    //low = ;
+    //scanf("%lf",&low);
+    low = -2;
 
     printf("Enter higher value of the integral\n");
     //change value here
-    scanf("%lf",&high);
-    //high = ;
+    //scanf("%lf",&high);
+    high = 1;
 
     printf("Enter total panels or 'n' value\n");
     //change value here
-    scanf("%d",&panels);
-    // panels = ;
+    //scanf("%d",&panels);
+    panels = 100;
 
     integration = simpsons(low,high,panels);
-    printf("Final Integration value is : %lf \n",integration);
+    printf("\nFinal Integration value is : %13.10lf \n\n",integration);
 }
 
+// Simpsons 1/3 rule calculating Function
 double simpsons(double low, double high, int panels)
 {
     double steps;
@@ -53,7 +54,7 @@ double simpsons(double low, double high, int panels)
             integration = integration + 4 * f(temp);
         }
         temp_int = integration * steps / 3;
-        printf("Integration at iteration %d is : %lf",i,temp_int);
+        printf("Integration at iteration %d is : %13.10lf",i,temp_int);
         printf("\n");
     }
     integration = integration * steps / 3;
